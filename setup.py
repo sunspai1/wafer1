120bf080e6e197596dd38afc42eb606c8d8fca6e
SUNNY PAI <sunsdatasci@gmail.com>
2021-08-18 22:21:12 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='fault detection project',
+    author='sunspai1',
+    license='MIT',
+)
